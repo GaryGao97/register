8a1ba8c53f781dd177d771198e7b956c413fcc45
yu.gy <yu.gy@foxmail.com>
2021-08-04 22:23:23 +0800

    # 统一ip

--- src/main/resources/static/js/lay-module/customize/globalConfig.js
+++ src/main/resources/static/js/lay-module/customize/globalConfig.js
@@ -1,6 +1,6 @@
 layui.define(function (exports) {
     const obj = {
-        api: "http://127.0.0.1:8936/api",
+        api: "http://localhost:8936/api",
     };
 
     exports('globalConfig', obj);
